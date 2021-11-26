dd4c1419544d1bd78a86ff033c7c20d425000f74
Jesse Houwing <jesse.houwing@gmail.com>
2021-11-26 22:22:15 +0100

    Reduce noise and reset the right folder locally

--- build.ps1
+++ build.ps1
@@ -91,14 +91,16 @@ $extensionManifest.contributions = @()
 
 if (Test-Path -path azure-pipelines-tasks)
 {
-    & git pull --all
+    cd azure-pipelines-tasks
+    & git reset --hard
+    & git pull --all --quiet
 }
 else 
 {
-    & git clone https://github.com/microsoft/azure-pipelines-tasks.git
+    & git clone https://github.com/microsoft/azure-pipelines-tasks.git --quiet
+    cd azure-pipelines-tasks
 }
 
-cd azure-pipelines-tasks
 git config --local pager.branch false
 $branches = & git branch -r
 $version = (($branches | Select-String -pattern "(?<=origin/releases/m)\d+$").Matches) | %{ [int32]$_.Value } | measure-object -maximum
